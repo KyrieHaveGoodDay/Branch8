228ae55caee4f8a4430be9d199bb343ae34a9a91
kyriehavegoodday <healthymbear@gmail.com>
2021-03-01 23:21:28 +0800

    串接資料完成

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(function(){
-    var len = 130; // 超過50個字以"..."取代
+    var len = 50; // 超過50個字以"..."取代
     $(".JQellipsis").each(function(i){
         if($(this).text().length>len){
             $(this).attr("title",$(this).text());
@@ -26,3 +26,42 @@ new Vue ({
         }
     }
 });
+
+new Vue ({
+    el:"#events",
+    data:{
+        datas:null
+    },
+
+    mounted() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET","https://fakerapi.it/api/v1/custom?title=city&image=image&description=text&date=dateTime&phone=phone&tag1=pokemon&tag2=pokemon&tag3=pokemon&address=streetAddress",true)
+        xhr.send();
+        xhr.onload = () => {
+            var obj = JSON.parse(xhr.responseText);
+            this.datas = obj.data;
+            // console.log(datas);
+        }
+    }
+});
+
+new Vue ({
+    el:"#news-box",
+    data:{
+        datas:null
+    },
+
+    mounted() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET","https://fakerapi.it/api/v1/custom?title=city&image=image&description=text&date=dateTime&phone=phone&tag1=pokemon&tag2=pokemon&tag3=pokemon&address=streetAddress",true)
+        xhr.send();
+        xhr.onload = () => {
+            var obj = JSON.parse(xhr.responseText);
+            this.datas = obj.data;
+            // console.log(datas);
+        }
+    }
+});
+
+
+
